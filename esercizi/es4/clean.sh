e592e7b84917478cddeec15f2e79a35f979ffd2f
ga <gabriele.mno@gmail.com>
2020-01-12 12:19:33 +0100

    commit

--- esercizi/es4/clean.sh
+++
@@ -1,7 +0,0 @@
-rm -rf *epot*
-rm -rf *ekin*
-rm -rf *etot*
-rm -rf *temp*
-rm -rf *press*
-rm -rf frames/*.xyz
-rm -rf *.o
--- esercizi/es7/Excercise/script.sh
+++
@@ -1,5 +0,0 @@
-for((counter = 0; counter < 3; counter++))
-do
-echo "$counter" > txt
-./Monte_Carlo_NVT.exe
-done
